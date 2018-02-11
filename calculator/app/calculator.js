0d16dc449e67bfe7fb8b446f8296d78f38f9eaf4
Johan Karlsson <johan.karlsson2@sj.se>
2018-02-11 23:13:55 +0100

    Removed old logic to clean up

--- calculator/app/calculator.js
+++
@@ -1,5 +0,0 @@
-function sum(var1, var2) {
-    return var1 + var2;
-}
-
-module.exports.sum = sum;
--- calculator/app/index.js
+++
@@ -1,5 +0,0 @@
-var calc = require('./calculator');
-
-var result = calc.sum(1, 1);
-
-console.log(result);
